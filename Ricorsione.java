fb5a42d89b030c4d0cd53bacc582709051d7bcd0
xfrancy <bortolettifrancesco@gmail.com>
2026-09-11 09:04:05 +0200

    finito

--- Ricorsione.java
+++ Ricorsione.java
@@ -1,5 +1,115 @@
 public class Ricorsione {
-    public static void main(String[] args) {
-        
-    }
+	/*
+	Es 1
+	*/
+	public static String inverti (String s) {
+		String invertita;
+		if (s.equals(""))invertita = "";
+		else invertita = s.charAt(s.length() -1) + inverti(s.substring(0, s.length() -1));
+		return invertita;
+	}
+	
+	/*
+	Es 2
+	*/
+	public static boolean palindroma(String s) {
+		boolean pal;
+		if (s.isEmpty() || s.length() == 1) pal = true;
+		else pal = s.charAt(0) == s.charAt(s.length() -1) && palindroma(s.substring(1,s.length() -1));
+        return pal;
+	}
+	
+	/*
+	Es 3
+	*/
+	public static int potenza(int b, int e) {
+		int risultato;
+		if (e == 0) risultato = 1;
+		else risultato = b * potenza(b, e -1);
+	    return risultato;
+	}
+	
+	/*
+	Es 4
+	*/
+	public static int numeroCifre(int n) {
+		int lunghezza;
+		if (n/10 < 1) lunghezza = 1;
+		else lunghezza = 1 + numeroCifre(n/10);
+	    return lunghezza;
+	}
+	
+	/*
+	Es 5
+	*/
+	public static boolean cerca(int dati[], int target, int pointer) {
+		boolean trovato;
+		if (pointer == dati.length) trovato = false;
+		else trovato = (dati[pointer] == target) || cerca(dati, target, pointer +1);
+	    return trovato;
+	}
+    
+    	/*
+	TESTER: NON MODIFICARE
+	*/
+	public static void main(String[] args) {
+		System.out.println("CONTROLLO SU inverti...");
+		System.out.print("Controllo che 'Ciao' diventi 'oaiC'... ");
+		if (inverti("Ciao").equals("oaiC")) {
+		    System.out.println("OK");
+		} else {
+		    System.out.println("ERRORE!");
+		}
+		System.out.print("Controllo che '' rimanga ''... ");
+		if (inverti("").equals("")) {
+		    System.out.println("OK");
+		} else {
+		    System.out.println("ERRORE!");
+		}
+		
+		System.out.println("\nCONTROLLO SU palindroma...");
+		System.out.print("Controllo che 'AA000AA' restituisca true... ");
+		if (palindroma("AA000AA")) {
+		    System.out.println("OK");
+		} else {
+		    System.out.println("ERRORE!");
+		}
+		System.out.print("Controllo che 'AA100AA' restituisca false... ");
+		if (!palindroma("AA100AA")) {
+		    System.out.println("OK");
+		} else {
+		    System.out.println("ERRORE!");
+		}
+		
+		System.out.println("\nCONTROLLO SU potenza...");
+		System.out.print("Controllo che 2^8 restituisca 256... ");
+		if (potenza(2,8) == 256) {
+		    System.out.println("OK");
+		} else {
+		    System.out.println("ERRORE!");
+		}
+		
+		System.out.println("\nCONTROLLO SU numeroCifre...");
+		System.out.print("Controllo che 20261110 restituisca 8... ");
+		if (numeroCifre(20261110) == 8) {
+		    System.out.println("OK");
+		} else {
+		    System.out.println("ERRORE!");
+		}
+		
+		int a[] = {10,20,30,40,50,60,70,80,90,100};
+		System.out.println("\nCONTROLLO SU cerca...");
+		System.out.print("Controllo se 60 esiste nell'array a... ");
+		if (cerca(a,60,0)) {
+		    System.out.println("OK");
+		} else {
+		    System.out.println("ERRORE!");
+		}
+		System.out.print("Controllo se 65 esiste nell'array a... ");
+		if (!cerca(a,65,0)) {
+		    System.out.println("OK");
+		} else {
+		    System.out.println("ERRORE!");
+		}		
+	}
 }
